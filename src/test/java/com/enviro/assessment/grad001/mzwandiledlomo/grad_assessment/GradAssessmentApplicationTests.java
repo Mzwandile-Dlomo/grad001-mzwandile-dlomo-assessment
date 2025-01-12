d7f70f7fbc81fb41bd3930374aadb970c42bfa3a
Mzwandile Dlomo <world.mjdlomo@gmail.com>
2025-01-12 13:10:24 +0200

    init

---
+++ src/test/java/com/enviro/assessment/grad001/mzwandiledlomo/grad_assessment/GradAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.mzwandiledlomo.grad_assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
